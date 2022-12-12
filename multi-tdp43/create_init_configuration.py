f5d15abc5e8533375dcac8b447dd2a708ea36e8f
ezippo <emanuele.zippo@gmail.com>
2022-12-12 13:47:06 +0100

    introduction of multi tdp simulation

---
+++ multi-tdp43/create_init_configuration.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys,os
+import itertools
+import numpy as np
+import gsd, gsd.hoomd
+
+import hoomd_util as hu
+
+# UNITS: distance -> nm   (!!!positions and sigma in files are in agstrom!!!)
+#        mass -> amu
+#        energy -> kJ/mol
+# ### MACROs
+box_length=200
+spacing = 25
+n_tdp43s = 200
+
+stat_file = 'input_stats/stats_module.dat'
+filein_ck1d = 'input_stats/CA_ck1delta.pdb'
+filein_tdp43 = 'input_stats/CA_TDP-43_261truncated.pdb'
+
+# ------------------------- MAIN -------------------------------
+
+if __name__=='__main__':
+    # Input parameters for all the amino acids 
+    aa_param_dict = hu.aa_stats_from_file(stat_file)
+    aa_type = list(aa_param_dict.keys())    
+    aa_mass = []
+    aa_charge = []
+    aa_sigma = []
+    aa_lambda = []
+    for k in aa_type:
+        aa_mass.append(aa_param_dict[k][0])
+        aa_charge.append(aa_param_dict[k][1])
+        aa_sigma.append(aa_param_dict[k][2])
+        aa_lambda.append(aa_param_dict[k][3])
+    
+    # Now we can translate the entire sequence into a sequence of numbers and 
+    # assign to each a.a. of the sequence its stats
+    ck1d_id, ck1d_mass, ck1d_charge, ck1d_sigma, ck1d_pos = hu.aa_stats_sequence(filein_ck1d, aa_param_dict)
+    ck1d_pos_arr = np.array(ck1d_pos)/10.
+    ck1d_sigma_arr = np.array(ck1d_sigma)/10.
+    ck1d_length = len(ck1d_id)       
+    ck1d_tot_mass = np.sum(ck1d_mass)   
+    ck1d_cof_pos = ( np.sum(ck1d_pos_arr[:,0]*ck1d_mass)/ck1d_tot_mass , np.sum(ck1d_pos_arr[:,1]*ck1d_mass)/ck1d_tot_mass , np.sum(ck1d_pos_arr[:,2]*ck1d_mass)/ck1d_tot_mass  )
+    ck1d_rel_pos = ck1d_pos_arr - ck1d_cof_pos
+    
+    tdp43_id, tdp43_mass, tdp43_charge, tdp43_sigma, tdp43_pos = hu.aa_stats_sequence(filein_tdp43, aa_param_dict)
+    tdp43_pos_arr = np.array(tdp43_pos)/10.
+    tdp43_pos_arr = tdp43_pos_arr + 7.
+    tdp43_sigma_arr = np.array(tdp43_sigma)/10.
+    tdp43_length = len(tdp43_id)
+    tdp43_mean_pos = ( np.sum(tdp43_pos_arr[:,0])/tdp43_length , np.sum(tdp43_pos_arr[:,1])/tdp43_length , np.sum(tdp43_pos_arr[:,2])/tdp43_length  )
+    tdp43_rel_pos = tdp43_pos_arr - tdp43_mean_pos
+    print(ck1d_length)
+    print(tdp43_length)
+
+    # positions
+    xx = [-box_length/2 +20 +25*i for i in range(7)]
+    cpos = list(itertools.product(xx, repeat=3))
+    cpos = cpos[:n_tdp43s+1]
+    positions = []
+    for x in cpos[:-1]:
+        tmp = list(tdp43_rel_pos+x)
+        positions = positions + tmp
+    positions = positions + [list(cpos[-1])]
+    print(len(positions))
+    print(len([positions[-1]]+ positions[:-1]))
+    
+    # ck1d moment of inertia
+    I = hu.protein_moment_inertia(ck1d_rel_pos, ck1d_mass)
+    I_diag, E_vec = np.linalg.eig(I)
+    ck1d_diag_pos = np.dot( E_vec.T, ck1d_rel_pos.T).T
+    I_check = hu.protein_moment_inertia(ck1d_diag_pos, ck1d_mass)  #check
+    
+    # Initialize bond
+    bond_pairs=[]
+    for i_tdp in range(n_tdp43s):
+        bond_pairs = bond_pairs + [[i_tdp*tdp43_length+1+i, i_tdp*tdp43_length+1+i+1] for i in range(tdp43_length-1)]
+
+    print(len(bond_pairs))
+    # Now we can build HOOMD data structure for one single frame
+    s=gsd.hoomd.Snapshot()
+    s.particles.N = n_tdp43s*tdp43_length + 1
+    s.particles.types = aa_type+['R']
+    s.particles.typeid = [len(aa_type)] + tdp43_id*n_tdp43s
+    s.particles.mass = [ck1d_tot_mass] + tdp43_mass*n_tdp43s
+    s.particles.charge = [0] + tdp43_charge*n_tdp43s
+    s.particles.position = [positions[-1]] + positions[:-1]
+    s.particles.moment_inertia = [I_diag[0], I_diag[1], I_diag[2]] + [0,0,0]*tdp43_length*n_tdp43s 
+    s.particles.orientation = [(1, 0, 0, 0)] * (n_tdp43s*tdp43_length+1)
+    s.particles.body = [0] + [-1]*tdp43_length*n_tdp43s
+    
+    s.bonds.N = len(bond_pairs)
+    s.bonds.types = ['AA_bond']
+    s.bonds.typeid = [0]*len(bond_pairs)
+    s.bonds.group = bond_pairs
+    
+    s.configuration.dimensions = 3
+    s.configuration.box = [box_length,box_length,box_length,0,0,0] 
+    s.configuration.step = 0
+    
+    with gsd.hoomd.open(name='input_stats/ck1d-center_multi-tdp43_start.gsd', mode='wb') as fout:
+        fout.append(s)
+        
+    '''
+    # create rigid body    
+    
+    sim = hoomd.Simulation(device=hoomd.device.CPU())
+    sim.create_state_from_gsd(filename='input_stats/ck1d-center_multi-tdp43_start.gsd')
+    
+    rigid = hoomd.md.constrain.Rigid()
+    rigid.body["R"] = {
+        "constituent_types": [aa_type[ck1d_id[i]] for i in range(ck1d_length)],
+        "positions": ck1d_rel_pos,
+        "orientations": [(1,0,0,0)]*ck1d_length,
+        "charges": ck1d_charge,
+        "diameters": [0.0]*ck1d_length
+        }
+
+    rigid.create_bodies(sim.state)
+    
+    integrator = hoomd.md.Integrator(dt=0.005, integrate_rotational_dof=True)
+    integrator.rigid = rigid
+    sim.operations.integrator = integrator
+    
+    sim.run(0)
+    hoomd.write.GSD.write(state=sim.state, filename='input_stats/ck1d-rigid_multi-tdp43_start.gsd')
+    '''
+    
---
+++ multi-tdp43/thermalize_ck1d_multi-tdp43.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys,os
+import time
+import numpy as np
+import hoomd
+import gsd, gsd.hoomd
+
+import hoomd_util as hu
+
+# UNITS: distance -> nm   (!!!positions and sigma in files are in agstrom!!!)
+#        mass -> amu
+#        energy -> kJ/mol
+
+# ### MACROs
+# Simulation parameters
+production_dt=0.01       # Time step for production run in picoseconds
+therm_steps=2000   # Total number of steps 
+production_T=300         # Temperature for production run in Kelvin
+temp = production_T*0.00831446      # Temp is RT [kJ/mol]
+box_lenght=200
+seed = 4567     #np.random.randint(0, 65535) 
+
+
+# Files
+stat_file = 'input_stats/stats_module.dat'
+filein_ck1d = 'input_stats/CA_ck1delta.pdb'
+#ex_number = sys.argv[1]
+ex_number = 0
+file_start = 'input_stats/ck1d-rigid_multi-tdp43_start.gsd'
+logfile = 'therm_ck1d-rigid_multi-tdp43_exl'+str(ex_number)
+
+# Logging time interval
+dt_dump = 200
+dt_log = 1000
+dt_backup = 1000000
+
+
+# ### CUSTOM ACTIONS
+class PrintTimestep(hoomd.custom.Action):
+
+    def __init__(self, t_start):
+        self._t_start = t_start
+
+    def act(self, timestep):
+        current_time = time.time()
+        current_time = current_time - self._t_start
+        print(f"Elapsed time {current_time} | Step {timestep}/{therm_steps} " )
+
+
+
+# --------------------------- MAIN ------------------------------
+
+if __name__=='__main__':
+    # TIME START
+    time_start = time.time()
+
+    # ### Input parameters for all the amino acids 
+    aa_param_dict = hu.aa_stats_from_file(stat_file)
+    aa_type = list(aa_param_dict.keys())
+    aa_mass = []
+    aa_charge = []
+    aa_sigma = []
+    aa_lambda = []
+    for k in aa_type:
+        aa_mass.append(aa_param_dict[k][0])
+        aa_charge.append(aa_param_dict[k][1])
+        aa_sigma.append(aa_param_dict[k][2]/10.)
+        aa_lambda.append(aa_param_dict[k][3])
+
+    ck1d_id, ck1d_mass, ck1d_charge, ck1d_sigma, ck1d_pos = hu.aa_stats_sequence(filein_ck1d, aa_param_dict)
+    ck1d_pos_arr = np.array(ck1d_pos)/10.
+    ck1d_sigma_arr = np.array(ck1d_sigma)/10.
+    ck1d_length = len(ck1d_id)       
+    ck1d_tot_mass = np.sum(ck1d_mass)   
+    ck1d_cof_pos = ( np.sum(ck1d_pos_arr[:,0]*ck1d_mass)/ck1d_tot_mass , np.sum(ck1d_pos_arr[:,1]*ck1d_mass)/ck1d_tot_mass , np.sum(ck1d_pos_arr[:,2]*ck1d_mass)/ck1d_tot_mass  )
+    ck1d_rel_pos = ck1d_pos_arr - ck1d_cof_pos
+    
+    # ### HOOMD3 routine
+    # ## INITIALIZATION
+    device = hoomd.device.CPU()
+    sim = hoomd.Simulation(device=device, seed=seed)    
+    sim.create_state_from_gsd(filename=file_start)
+    snap = sim.state.get_snapshot()
+    ck1d_mass = snap.particles.mass[0]
+    
+    # # rigid body
+    rigid = hoomd.md.constrain.Rigid()
+    rigid.body['R'] = {
+        "constituent_types": [aa_type[ck1d_id[i]] for i in range(ck1d_length)],
+        "positions": ck1d_rel_pos,
+        "orientations": [(1,0,0,0)]*ck1d_length,
+        "charges": ck1d_charge,
+        "diameters": [0.0]*ck1d_length
+        }
+    
+    # # groups
+    all_group = hoomd.filter.All()
+    moving_group = hoomd.filter.Rigid(("center", "free"))
+
+    # gaussian distributed initial velocities 
+    sim.state.thermalize_particle_momenta(filter=all_group, kT=temp)
+    
+    # ## PAIR INTERACTIONS
+    cell = hoomd.md.nlist.Cell(buffer=0.4, exclusions=('bond', 'body'))
+    
+    # # bonds
+    harmonic = hoomd.md.bond.Harmonic()
+    harmonic.params['AA_bond'] = dict(k=8360, r0=0.381)
+    
+    # # electrostatics forces
+    yukawa = hoomd.md.pair.Yukawa(nlist=cell)
+    for i in range(len(aa_type)):
+        atom1 = aa_type[i]
+        for j in range(i,len(aa_type)):
+            atom2 = aa_type[j]
+            yukawa.params[(atom1,atom2)] = dict(epsilon=aa_charge[i]*aa_charge[j]*1.73136, kappa=1.0)
+            yukawa.r_cut[(atom1,atom2)] = 3.5
+        yukawa.params[(atom1,'R')] = dict(epsilon=0, kappa=1.0)
+        yukawa.r_cut[(atom1,'R')] = 0.0
+    yukawa.params[('R','R')] = dict(epsilon=0, kappa=1.0)
+    yukawa.r_cut[('R','R')] = 0.0
+    
+    # # nonbonded: ashbaugh-hatch potential
+    ashbaugh_table = hoomd.md.pair.Table(nlist=cell)
+    for i in range(len(aa_type)):
+        atom1 = aa_type[i]
+        for j in range(i,len(aa_type)):
+            atom2 = aa_type[j]
+            Ulist = hu.Ulist_ashbaugh(sigma=[aa_sigma[i], aa_sigma[j]], 
+                                      lambda_hps=[aa_lambda[i], aa_lambda[j]],
+                                      r_max=2.0, r_min=0.2, n_bins=100000, epsilon=0.8368)
+            Flist = hu.Flist_ashbaugh(sigma=[aa_sigma[i], aa_sigma[j]], 
+                                      lambda_hps=[aa_lambda[i], aa_lambda[j]],
+                                      r_max=2.0, r_min=0.2, n_bins=100000, epsilon=0.8368)
+            ashbaugh_table.params[(atom1, atom2)] = dict(r_min=0.2, U=Ulist, F=Flist)
+            ashbaugh_table.r_cut[(atom1, atom2)] = 2.0            
+        ashbaugh_table.params[(atom1, 'R')] = dict(r_min=0., U=[0], F=[0])
+        ashbaugh_table.r_cut[(atom1, 'R')] = 0 
+    ashbaugh_table.params[('R', 'R')] = dict(r_min=0., U=[0], F=[0])
+    ashbaugh_table.r_cut[('R', 'R')] = 0 
+    
+    # ## INTEGRATOR
+    integrator = hoomd.md.Integrator(production_dt, integrate_rotational_dof=True)        
+    # method : Langevin
+    langevin = hoomd.md.methods.Langevin(filter=moving_group, kT=temp)
+    for i,name in enumerate(aa_type):
+        langevin.gamma[name] = aa_mass[i]/1000.0
+        langevin.gamma_r[name] = (0.0, 0.0, 0.0)
+    langevin.gamma['R'] = ck1d_mass/1000.0
+    langevin.gamma_r['R'] = (1.0, 1.0, 1.0)
+    # constraints : rigid body
+    integrator.rigid = rigid
+    # forces 
+    integrator.forces.append(harmonic)
+    integrator.forces.append(yukawa)
+    integrator.forces.append(ashbaugh_table)
+    integrator.methods.append(langevin)
+    
+    # ## LOGGING
+    # dump files
+    dump_gsd = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(dt_dump), 
+                               filename=logfile+'_dump.gsd', filter=all_group,
+                               dynamic=['property', 'momentum', 'attribute', 'topology'])                  # you can add [attributes(particles/typeid)] to trace phosphorylation
+
+    # back-up files
+    sim_info_log = hoomd.logging.Logger()
+    sim_info_log.add(sim)
+    backup1_gsd = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(dt_backup), 
+                                  filename='restart_tmp1_exl'+str(ex_number)+'.gsd', filter=all_group,
+                                  mode='wb', truncate=True, log=sim_info_log)
+    backup2_gsd = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(dt_backup, phase=int(dt_backup/2.)), 
+                                  filename='restart_tmp2_exl'+str(ex_number)+'.gsd', filter=all_group,
+                                  mode='wb', truncate=True, log=sim_info_log)
+    
+    
+    # thermodynamical quantities
+    therm_quantities = hoomd.md.compute.ThermodynamicQuantities(filter=all_group)
+    tq_log = hoomd.logging.Logger()
+    tq_log.add(therm_quantities)
+    tq_gsd = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(dt_log), 
+                             filename=logfile+'_log.gsd', filter=hoomd.filter.Null(),
+                             log=tq_log)
+    
+    # # Custom action
+    print(f"Initial time: {time.time()-time_start}")
+    time_start = time.time()
+    time_action = PrintTimestep(time_start)
+    time_writer = hoomd.write.CustomWriter(action=time_action, trigger=hoomd.trigger.Periodic(100))
+    
+    # ## SET SIMULATION OPERATIONS
+    sim.operations.integrator = integrator 
+    sim.operations.computes.append(therm_quantities)
+
+    sim.operations.writers.append(dump_gsd)
+    sim.operations.writers.append(backup1_gsd)
+    sim.operations.writers.append(backup2_gsd)
+    sim.operations.writers.append(tq_gsd)
+    sim.operations += time_writer
+
+    sim.run(therm_steps)
+    
