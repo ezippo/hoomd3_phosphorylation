aa61754eca386b52d53775a03c3e86d04679ade8
ezippo <71833592+ezippo@users.noreply.github.com>
2024-10-04 15:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if __name__=='__main__':
     parser.add_argument('-i','--infile', required=True, type=str, help='Input file with simulation parameters, logging file name and parameters, system file name.')
     parser.add_argument('-r', '--rescale', default=0, type=float, help='Scale down rigid body interaction by X percentage. To use also in create_initial_configuration mode to incude the rescaled rigid body types (value of argmuent not important in this case).')
     parser.add_argument('-br', '--boxresize', default=None, nargs=3, type=float, help='The simulation will be used to resize the box from the initial configuration to the sizes given in the argument. The argument should be a list with the side lengths (Lx, Ly, Lz).')
-    parser.add_argument('-d', '--displactivesite', default=None, type=str, help='To use in case of displacement of active site compared to the positions of the beads. Give as argument the path to the file with xyz coordinates of the displaced active site in Angstrom. Displacement compared to the positions in the pdb given in the infile. ')
 
     parser.add_argument('--mode', default='relax', type=str, choices=['relax', 'ness', 'nophospho'], help='Default phosphorylation is active without exchange SER/SEP with the chemical bath. If ness also exchange step is added. If nophospho phosphorzlation and exchange are deactivated.' )
     
